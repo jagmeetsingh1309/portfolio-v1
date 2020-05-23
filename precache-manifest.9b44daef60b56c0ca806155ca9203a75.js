d39ac56e28b61a69d9fda93d0e881e2cd6450ee7
jagmeet singh <jagmeet.alagh@gmail.com>
2020-05-23 16:49:50 +0530

    Updates

---
+++ precache-manifest.9b44daef60b56c0ca806155ca9203a75.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eec0dd73d762b3a69564f8489c5adb52",
+    "url": "/portfolio-v1/index.html"
+  },
+  {
+    "revision": "d08df6b4628347ec5a03",
+    "url": "/portfolio-v1/static/css/main.a5486559.chunk.css"
+  },
+  {
+    "revision": "0d0f13ecbbc590a16870",
+    "url": "/portfolio-v1/static/js/2.a82beb66.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio-v1/static/js/2.a82beb66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d08df6b4628347ec5a03",
+    "url": "/portfolio-v1/static/js/main.b2862ff7.chunk.js"
+  },
+  {
+    "revision": "7e4938f74b012e12d07a",
+    "url": "/portfolio-v1/static/js/runtime-main.3073c16e.js"
+  },
+  {
+    "revision": "79ea9836387ee55b61c9a3a21b3ab0f0",
+    "url": "/portfolio-v1/static/media/me.79ea9836.png"
+  },
+  {
+    "revision": "517dc48e8a93f2cf93f58258f2b3cce8",
+    "url": "/portfolio-v1/static/media/project-1.517dc48e.png"
+  },
+  {
+    "revision": "c26deafd275492a405dc0b4aedf9361e",
+    "url": "/portfolio-v1/static/media/project-2.c26deafd.png"
+  }
+]);
